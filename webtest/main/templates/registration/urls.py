1e159580946c3c170c257c8d2658b280159cc513
JohnKennedy1963 <kobelevruslan9@gmail.com>
2023-05-07 01:24:16 +0300

    small changes

--- webtest/main/templates/registration/urls.py
+++ webtest/main/templates/registration/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 import sys
-sys.path.append(r'C:\Users\abcdefg\Desktop\TestWeb-main\webtest\main')
+sys.path.append(r'C:\Users\abcdefg\Documents\GitHub\TestWeb\webtest\main')
 from views import register
 
 urlpatterns = [
