294d9c63ecb4e31bd6271cd7a48b6473e1961049
kepusttt <kobelevruslan9@gmail.com>
2023-06-02 06:57:15 +0300

    i wanna kms

--- webtest/main/templates/registration/urls.py
+++ webtest/main/templates/registration/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 import sys
-sys.path.append(r'C:\Users\Bound\Downloads\TestWeb-main\webtest\main')
+sys.path.append(r'TestWeb\webtest\main')
 # sys.path.append(r'C:\Users\abcdefg\Documents\GitHub\TestWeb\webtest\main')
-from views import register
+from main.views import register
 
 urlpatterns = [
     path('register/', register, name='register'),
